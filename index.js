c3696e7be90768f819737e1d5c3b199d72b6a274
Chandrababu Gowda <96734849+chandrababu-gowda@users.noreply.github.com>
2022-10-26 13:43:16 +0530

    Show the user selected value

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let selected = document.querySelectorAll('.selection');
-let generatedText = document.querySelector('.computer-generated')
+let generatedText = document.querySelector('.computer-generated');
+let selectedText = document.querySelector('.user-selection-display')
 
 selected.forEach(ele => {
     ele.addEventListener('click',()=>{
+        selectedText.innerHTML = `<h1>${ele.innerText}</h1>`
         let generatedNum = Math.floor(Math.random()*3)+1;
         switch (generatedNum) {
             case 1:
